90b1893ab543b3c75c8b1567592896d22e6484dc
Dikontay <dalimbayeva@icloud.com>
2024-03-06 13:48:38 +0500

    json customization

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"greenlight/internal/data"
 	"net/http"
 	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Title   string   `json : "title"`
+		Year    int32    `json : "year"`
+		Runtime int32    `json : "runtime"`
+		Genres  []string `json : "genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
+	if err != nil {
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -28,7 +45,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	err = app.writeJSON(w, http.StatusOK, movie, nil)
 
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
